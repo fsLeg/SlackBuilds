a58973f61e7a13018efe48c39a6ab664d1ff040b
fsLeg <fsleg@t-rg.ws>
2014-12-03 19:16:27 +0300

    Opera: switched to Stable stream by default. Upped version to 26.0.1656.32. Upped local FFmpeg version to 2.4.4.

--- opera/opera.sh
+++ opera/opera.sh
@@ -2,19 +2,19 @@
 
 # Allow users to override command-line options
 # Based on Gentoo's chromium package (and by extension, Debian's)
-if [[ -f /etc/default/opera-beta ]]; then
-	. /etc/default/opera-beta
+if [[ -f /etc/default/opera_FLAVOR_ ]]; then
+	. /etc/default/opera_FLAVOR_
 fi
 
 # Prefer user defined OPERA_USER_FLAGS (from env) over system
-# default OPERA_FLAGS (from /etc/default/opera-beta)
+# default OPERA_FLAGS (from /etc/default/opera_FLAVOR_)
 OPERA_FLAGS=${OPERA_USER_FLAGS:-$OPERA_FLAGS}
 
 export CHROME_WRAPPER=$(readlink -f "$0")
-export CHROME_DESKTOP=opera-beta.desktop
+export CHROME_DESKTOP=opera_FLAVOR_.desktop
 
 # Use ffmpeg libs
 export LD_LIBRARY_PATH="PATH_TO_FFMPEG:${LD_LIBRARY_PATH}"
 
-exec /usr/lib_LIBDIRSUFFIX_/opera-beta/opera-beta $OPERA_FLAGS "$@"
+exec /usr/lib_LIBDIRSUFFIX_/opera_FLAVOR_/opera_FLAVOR_ $OPERA_FLAGS "$@"
 
