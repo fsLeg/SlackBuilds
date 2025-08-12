8466087b07b4b0088980f7ee819ac450a4278aee
fsLeg <fsleg@t-rg.ws>
2025-08-12 23:45:28 +0300

    element-desktop: updated to version 1.11.109; more vendoring optimizations

--- element-desktop/mkvendor.sh
+++ element-desktop/mkvendor.sh
@@ -46,21 +46,12 @@ cd ../element-desktop-$VERSION
 
 ## pre-built electron
 EVERSION=$(jq --raw-output '.devDependencies.electron' < package.json)
-mkdir -p $XDG_CACHE_HOME/electron{,-builder/fpm@2.0.1/fpm@2.0.1-fpm-1.16.0-ruby-3.4.3-linux-amd64}
+mkdir -p $XDG_CACHE_HOME/electron{,-builder}
 if [ -e $CWD/electron-v$EVERSION-linux-x64.zip ]; then
   cp $CWD/electron-v$EVERSION-linux-x64.zip $XDG_CACHE_HOME/electron/
 else
   wget --directory-prefix=$XDG_CACHE_HOME/electron --tries=0 --retry-on-http-error=503 https://github.com/electron/electron/releases/download/v$EVERSION/electron-v$EVERSION-linux-x64.zip
 fi
-
-## pre-built ruby for electron-builder
-if [ -e $CWD/fpm-1.16.0-ruby-3.4.3-linux-amd64.7z ]; then
-  cp $CWD/fpm-1.16.0-ruby-3.4.3-linux-amd64.7z $XDG_CACHE_HOME/electron-builder/
-else
-  wget --directory-prefix=$XDG_CACHE_HOME/electron-builder --tries=0 --retry-on-http-error=503 https://github.com/electron-userland/electron-builder-binaries/releases/download/fpm@2.0.1/fpm-1.16.0-ruby-3.4.3-linux-amd64.7z
-fi
-7z x -o$XDG_CACHE_HOME/electron-builder/fpm@2.0.1/fpm@2.0.1-fpm-1.16.0-ruby-3.4.3-linux-amd64 $XDG_CACHE_HOME/electron-builder/fpm-1.16.0-ruby-3.4.3-linux-amd64.7z
-rm $XDG_CACHE_HOME/electron-builder/fpm-1.16.0-ruby-3.4.3-linux-amd64.7z
 
 ## element-desktop itself
 yarn install --frozen-lockfile \
@@ -72,6 +63,18 @@ EDIR=$(find $XDG_CACHE_HOME/electron -type d -mindepth 1 -maxdepth 1)
 rm $EDIR/electron-v$EVERSION-linux-x64.zip
 ln -s ../electron-v$EVERSION-linux-x64.zip $EDIR/
 
+## pre-built ruby for electron-builder
+FPM_RUBY=$(grep linux-amd64 node_modules/app-builder-lib/out/targets/tools.js | head -1 | cut -d'"' -f2)
+FPM_RUBY_TAG=$(grep 'const fpmPath' node_modules/app-builder-lib/out/targets/tools.js | head -1 | cut -d'"' -f2)
+mkdir -p $XDG_CACHE_HOME/electron-builder/$FPM_RUBY_TAG/$FPM_RUBY_TAG-${FPM_RUBY%.7z}
+if [ -e $CWD/$FPM_RUBY ]; then
+  cp $CWD/$FPM_RUBY $XDG_CACHE_HOME/electron-builder/
+else
+  wget --directory-prefix=$XDG_CACHE_HOME/electron-builder/ --tries=0 --retry-on-http-error=503 https://github.com/electron-userland/electron-builder-binaries/releases/download/$FPM_RUBY_TAG/$FPM_RUBY
+fi
+7z x -o$XDG_CACHE_HOME/electron-builder/$FPM_RUBY_TAG/$FPM_RUBY_TAG-${FPM_RUBY%.7z} $XDG_CACHE_HOME/electron-builder/$FPM_RUBY
+rm $XDG_CACHE_HOME/electron-builder/$FPM_RUBY
+
 ## matrix-seshat
 RUST_PLATFORM=$(rustc -Vv | awk '/host/ {print $2}')
 SESHATVERSION=$(jq --raw-output '.hakDependencies."matrix-seshat"' < package.json | tr -d '^')
@@ -82,9 +85,9 @@ else
   wget --directory-prefix=.hak --tries=0 --retry-on-http-error=503 https://github.com/matrix-org/seshat/archive/$SESHATVERSION/seshat-$SESHATVERSION.tar.gz
 fi
 tar xf .hak/seshat-$SESHATVERSION.tar.gz -C .hak seshat-$SESHATVERSION/seshat-node
-mv .hak/seshat-$SESHATVERSION/seshat-node .hak/matrix-seshat/$RUST_PLATFORM/build
-cp -R .hak/matrix-seshat/$RUST_PLATFORM/build .hak/hakModules/matrix-seshat
-rm .hak/seshat-$SESHATVERSION.tar.gz
+mv .hak/seshat-$SESHATVERSION/seshat-node .hak/hakModules/matrix-seshat
+ln -s ../../hakModules/matrix-seshat .hak/matrix-seshat/$RUST_PLATFORM/build
+rm -r .hak/seshat-$SESHATVERSION.tar.gz .hak/seshat-$SESHATVERSION
 
 cd .hak/matrix-seshat/$RUST_PLATFORM/build
 yarn install --frozen-lockfile \
@@ -117,8 +120,7 @@ cd ../../../..
 cd ..
 tar cfJ $CWD/element-desktop-$VERSION-vendored-sources.tar.xz \
            element-desktop-$VERSION/vendor \
-           element-desktop-$VERSION/.hak/hakModules \
-           element-desktop-$VERSION/.hak/matrix-seshat/$RUST_PLATFORM/build \
-           element-desktop-$VERSION/electron-cache/electron{,-builder}
+           element-desktop-$VERSION/.hak \
+           element-desktop-$VERSION/electron-cache
 rm -rf $TMP
 cd $CWD
